496b5cd490e80ccd8d5ecd839d9d6c34b2891441
TungThanhHoang <thanhtung1760@gmail.com>
2022-01-18 09:07:59 +0700

    deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <>
+    {/* Footer */}
       <section className="bg-grayyellowcustom ">
           <div className="max-w-screen-xl mx-auto h-16 px-2 flex items-center">
             <div className="text-2xs font-semibold text-center ">Designed by Thanh Tùng</div>
