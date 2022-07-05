8c0c533bab97d990e8cc9a269a6dc384cae79c82
TungThanhHoang <thanhtung1760@com.com>
2022-07-05 23:27:38 +0700

    fix: update seo

---
+++ src/components/ViewModalPDF.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewModalPDF = () => {
+    return (
+        <div>
+            <iframe src="https://drive.google.com/file/d/1wc9OzXM263EYj8r1UrmIIJx6Y-eoJJ54/view?usp=sharing" frameborder="0"></iframe>
+        </div>
+    )
+}
+
+export default ViewModalPDF
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "./index.css";
 ReactDOM.render(
   <React.StrictMode>
       <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
+  ,document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
